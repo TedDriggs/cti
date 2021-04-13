749c7142cbf2a416417c3a29a7d353a32d86e7d3
Ted Driggs <ted.driggs@outlook.com>
2021-04-13 08:57:00 -0700

    Expand built-in collection

--- stix/examples/custom_collection.rs
+++ stix/examples/custom_collection.rs
@@ -1,12 +1,22 @@
-use stix::{Identity, Relationship};
+#![allow(dead_code)]
+use stix::{AttackPattern, Identity, Malware, Relationship, Tool};
 
 #[derive(stix::Collection)]
 enum Declaration {
+    #[stix(rel(Uses, Tool), rel(Uses, Malware))]
+    AttackPattern(AttackPattern),
     Identity(Identity),
+    Malware(Malware),
     Relationship(Relationship),
+    Tool(Tool),
+}
+
+fn act_on_collection(coll: &Collection) {
+    for ap in coll.attack_patterns() {
+        for _malware in ap.uses_malwares() {}
+    }
 }
 
 fn main() {
-    let builder = CollectionBuilder::default();
-    println!("{}", builder.len());
+    let _builder = CollectionBuilder::default();
 }
