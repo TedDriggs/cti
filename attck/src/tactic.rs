457c4d5e8d0a0f3bdf7eefd8a992780f5d883eeb
Ted Driggs <ted.driggs@outlook.com>
2021-04-15 13:16:54 -0700

    Add missing properties to attck::Tactic

--- attck/src/tactic.rs
+++ attck/src/tactic.rs
@@ -6,6 +6,13 @@ use stix::CommonProperties;
 pub struct Tactic {
     #[serde(flatten)]
     common: CommonProperties,
+    #[serde(default)]
+    pub name: String,
+    #[serde(default)]
+    pub description: Option<String>,
+    /// The kebab-case name used in [`KillChainPhase`](::stix::KillChainPhase) references to this tactic.
+    #[serde(rename = "x_mitre_shortname")]
+    pub shortname: String,
 }
 
 impl AsRef<CommonProperties> for Tactic {
