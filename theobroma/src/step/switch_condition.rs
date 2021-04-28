ea53c2a406569a546a2b9040466c6682a3010e0a
Ted Driggs <ted.driggs@outlook.com>
2021-04-28 04:50:11 -0700

    Add getter for default case in switch step

--- theobroma/src/step/switch_condition.rs
+++ theobroma/src/step/switch_condition.rs
@@ -14,3 +14,12 @@ pub struct StepSwitchCondition {
     switch_var: Variable,
     cases: IndexMap<String, Vec<Id>>,
 }
+
+impl StepSwitchCondition {
+    /// Get the steps associated with the default case, if one was provided.
+    ///
+    /// The default case has the literal key `"default"`.
+    pub fn default_case(&self) -> Option<&[Id]> {
+        self.cases.get("default").map(|s| s.as_slice())
+    }
+}
